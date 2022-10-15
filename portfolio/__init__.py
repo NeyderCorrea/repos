7d07ec210517e967ff0135c8fde9a1cd5ccd9e09
andres <sistemas5adg@gmail.com>
2022-10-14 22:20:05 -0500

    arreglando heroku

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -5,14 +5,9 @@ def create_app():
     app = Flask(__name__)
 
     app.config.from_mapping(
-        SENDGRID_KEY=os.environ.get('SENDGRID_API_KEY'),
         FROM_MAIL=os.environ.get('FROM_MAIL'),
         MAIL_PASS=os.environ.get('MAIL_PASS'),
-        SECRET_KEY=os.environ.get('SECRET_KEY'),
-        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
-        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
-        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
-        DATABASE=os.environ.get('FLASK_DATABASE')
+        SECRET_KEY=os.environ.get('SECRET_KEY')
     )
 
     from . import portfolio
